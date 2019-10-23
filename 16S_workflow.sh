fa21a488062f5612a36cb7dc94c223bb3db368cc
lfourteau91@gmail.com <lfourteau91@gmail.com>
2019-10-23 19:22:00 +0200

    Filtrage fini ==> fastq générés

--- 16S_workflow.sh
+++ 16S_workflow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Projet 16S workflow"
+echo "----Projet 16S workflow----"
 
 # Definition des variables.
 dossier_reads=$1
@@ -10,16 +10,47 @@ dossier_sortie=$2
 fastqc_folder=software/FastQC/
 
 # Regarde si le dossier result est vide
-if [ -z "$(ls -A results/)" ]; then
+if [ -z "$(ls -A results/fastqc)" ]; then
     # 1. Appelle du logiciel fastqc et de filtrage
-    ./$fastqc_folder/fastqc $1/*_R1.fastq.gz $1/*_R2.fastq.gz -o $2
+    mkdir $2/fastqc
+    ./$fastqc_folder/fastqc $1/*_R1.fastq.gz $1/*_R2.fastq.gz -o $2/fastqc
 else
     echo "Le dossier results n'est pas vide."
     echo "Les fichiers sont déjà générés."
 fi
 
-# Definir le chemin pour le logiciel AlienTrimmer.
-alien_folder=data/soft/
+# Ungip tous les fichier dans data/fastq.
+if [ -e "data/fastq/*.gz" ]
+then
+    gunzip data/fastq/*.gz
+else
+    echo "Tous les fichier sont décompressés."
+fi
 
-# 2. Filtrer un les reads appariés a l'aide d'AlienTrimmer
-java -jar $alien_folder/AlienTrimmer.jar
+# Definir le chemin pour le logiciel AlienTrimmer et séquence Alien.
+alien_folder=data/soft/
+dir_seq_alien=data/databases/
+
+# Verification de l'existence results/alientrimmer.
+if [ -d $2/alientrimmer ]
+then
+    echo "alientrimmer exist."
+else
+    mkdir $2/alientrimmer
+fi
+
+# 2. Filtrer les reads appariés a l'aide d'AlienTrimmer
+if [ -e "results/alientrimmer/*.fastq" ]
+then
+    all_R1=`ls $1*_R1.fastq`
+    for i in $all_R1
+    do
+        j=$(echo $i|sed "s:R1:R2:g")
+        java -jar $alien_folder/AlienTrimmer.jar -if $i -ir $j -c $dir_seq_alien/contaminants.fasta -q 20 -of $2/alientrimmer/$(basename $i) -or $2/alientrimmer/$(basename $j)
+    done
+else
+    echo "Tous les fastq sont générés."
+    echo "Fin du filtrage de(s) read(s)"
+fi
+
+

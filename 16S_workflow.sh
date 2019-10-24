1b0cd76aeaabfa0b80e249a26fa252a6c1efc541
lfourteau91@gmail.com <lfourteau91@gmail.com>
2019-10-24 22:48:40 +0200

    final modification

--- 16S_workflow.sh
+++ 16S_workflow.sh
@@ -3,14 +3,21 @@
 echo "----Projet 16S workflow----"
 
 # Definition des variables.
-dossier_reads=$1
-dossier_sortie=$2
+echo $1
+echo $2
+
+if [ -d $2 ]
+then
+    echo "$2 exist."
+else
+    mkdir $2
+fi
 
 # Definir le lien pour le logiciel FastQC.
-fastqc_folder=software/FastQC/
+fastqc_folder=FastQC/
 
-# Regarde si le dossier result est vide
-if [ -z "$(ls -A results/fastqc)" ]; then
+# Regarde si le dossier result est vide.
+if [ -z "$(ls -A $2/fastqc)" ]; then
     # 1. Appelle du logiciel fastqc et de filtrage
     mkdir $2/fastqc
     ./$fastqc_folder/fastqc $1/*_R1.fastq.gz $1/*_R2.fastq.gz -o $2/fastqc
@@ -19,17 +26,13 @@ else
     echo "Les fichiers sont déjà générés."
 fi
 
-# Ungip tous les fichier dans data/fastq.
-if [ -e "data/fastq/*.gz" ]
-then
-    gunzip data/fastq/*.gz
-else
-    echo "Tous les fichier sont décompressés."
-fi
+# Ungip tous les fichier dans fastq.
+gunzip fastq/*.gz
+echo "Tous les fichier sont décompressés."
 
 # Definir le chemin pour le logiciel AlienTrimmer et séquence Alien.
-alien_folder=data/soft/
-dir_seq_alien=data/databases/
+alien_folder=soft/
+dir_seq_alien=databases/
 
 # Verification de l'existence results/alientrimmer.
 if [ -d $2/alientrimmer ]
@@ -40,18 +43,14 @@ else
 fi
 
 # 2. Filtrer les reads appariés a l'aide d'AlienTrimmer
-if [ -e "results/alientrimmer/*.fastq" ]
-then
-    all_R1=`ls $1*_R1.fastq`
-    for i in $all_R1
-    do
-        j=$(echo $i|sed "s:R1:R2:g")
-        java -jar $alien_folder/AlienTrimmer.jar -if $i -ir $j -c $dir_seq_alien/contaminants.fasta -q 20 -of $2/alientrimmer/$(basename $i) -or $2/alientrimmer/$(basename $j)
-    done
-else
-    echo "Tous les fastq sont générés."
-    echo "Fin du filtrage de(s) read(s)"
-fi
+all_R1=`ls $1/*_R1.fastq`
+for i in $all_R1
+do
+    j=$(echo $i|sed "s:R1:R2:g")
+    java -jar $alien_folder/AlienTrimmer.jar -if $i -ir $j -c $dir_seq_alien/contaminants.fasta -q 20 -of $2/alientrimmer/$(basename $i) -or $2/alientrimmer/$(basename $j)
+done
+echo "Tous les fastq sont générés."
+echo "Fin du filtrage de(s) read(s)"
 
 # Verification de l'existence results/fusion_reads.
 if [ -d $2/fusion_reads ]
@@ -61,9 +60,6 @@ else
     mkdir $2/fusion_reads
     echo "dossier fusion_reads créé"
 fi
-
-# Pour trouver de l'aide de vsearch.
-# ./$alien_folder/vsearch --help
 
 # 3 - fusionner les reads à l'aide de Vsearch (paired-end reads merging)
 # Et sort un fichier format fasta. + ajouter suffix a chaque read
@@ -75,7 +71,7 @@ then
     echo "Tous les fastq ont été fusionnés et ont généré des fastas."
     echo "Fin de la fusion de(s) fasta."
 else
-    all_R1=`ls $2alientrimmer/*_R1.fastq`
+    all_R1=`ls $2/alientrimmer/*_R1.fastq`
     for i in $all_R1
     do
         i_R1=$(basename $i)
@@ -85,7 +81,7 @@ else
         output=$(echo $pre_outj | sed "s/_R2//g")
 
         # paired-end reads mergin
-        ./$alien_folder/vsearch --fastq_mergepairs $2alientrimmer/$i_R1 --reverse $2alientrimmer/$j_R2  --fastaout $2fusion_reads/$output.fasta --label_suffix ";sample=$output"
+        ./$alien_folder/vsearch --fastq_mergepairs $2/alientrimmer/$i_R1 --reverse $2/alientrimmer/$j_R2  --fastaout $2/fusion_reads/$output.fasta --label_suffix ";sample=$output"
     done
 fi
 
@@ -99,8 +95,8 @@ else
 fi
 
 # Rassembler tous les fichiers en 1 seul ==> amplicon.fasta.
-cat $2fusion_reads/*.fasta > $2concatenation_amplicon/amplicon.fasta
-sed -i "s/ //g" $2concatenation_amplicon/amplicon.fasta
+cat $2/fusion_reads/*.fasta > $2/concatenation_amplicon/amplicon.fasta
+sed -i "s/ //g" $2/concatenation_amplicon/amplicon.fasta
 echo "La concatenation est complete -> amplicon.fasta"
 
 # 4 étapes de clusterisation.
@@ -116,7 +112,7 @@ else
     mkdir $2/dereplication
     echo "dossier dereplication créé"
 fi
-./$alien_folder/vsearch --derep_fulllength $2concatenation_amplicon/amplicon.fasta --sizeout --minuniquesize 10 --output $2dereplication/derep_amplicon.fasta
+./$alien_folder/vsearch --derep_fulllength $2/concatenation_amplicon/amplicon.fasta --sizeout --minuniquesize 10 --output $2/dereplication/derep_amplicon.fasta
 
 # 4-3 : Suppression des chimères : chimera detection
 # Approche denovo, confirmer avec blast ?
@@ -127,7 +123,7 @@ else
     mkdir $2/chimeras
     echo "dossier chimeras créé"
 fi
-./$alien_folder/vsearch --uchime_denovo $2dereplication/derep_amplicon.fasta --nonchimeras $2chimeras/chimeras.fasta
+./$alien_folder/vsearch --uchime_denovo $2/dereplication/derep_amplicon.fasta --nonchimeras $2/chimeras/chimeras.fasta
 
 # 4-3 : Clustering = 97 identité, OTU=centroids
 # Chaque centroid = OTU_numéro_de_séquence.
@@ -138,7 +134,7 @@ else
     mkdir $2/clustering
     echo "dossier clustering créé"
 fi
-./$alien_folder/vsearch --cluster_size $2chimeras/chimeras.fasta  --id 0.97 --relabel OTU_ --centroids $2clustering/clustering.fasta
+./$alien_folder/vsearch --cluster_size $2/chimeras/chimeras.fasta  --id 0.97 --relabel OTU_ --centroids $2/clustering/clustering.fasta
 
 # Maintenant que nous avons les OTUs:
 # 5-1 : Déterminer leurs abondances et retourner un table d'abondance.
@@ -149,15 +145,10 @@ else
     mkdir $2/table_abondance
     echo "dossier table_abondance créé"
 fi
-./$alien_folder/vsearch --usearch_global $2chimeras/chimeras.fasta --db $2clustering/clustering.fasta --id 0.97 --otutabout $2table_abondance/table_abondance
+./$alien_folder/vsearch --usearch_global $2/chimeras/chimeras.fasta --db $2/clustering/clustering.fasta --id 0.97 --otutabout $2/table_abondance/table_abondance
 
 # 5-2 : Annoter
-if [ -d $2/annotation ]
-then
-    echo "annontation exist."
-else
-    mkdir $2/annotation
-    echo "dossier annotation créé"
-fi
-./$alien_folder/vsearch --usearch_global $2clustering/clustering.fasta --db data/databases/mock_16S_18S.fasta --id 0.9 --top_hits_only --userfields query+target --userout $2table_abondance/table_abondance
-sed '1iOTU\tAnnotation' -i $2table_abondance/table_abondance
+./$alien_folder/vsearch --usearch_global $2/clustering/clustering.fasta --db databases/mock_16S_18S.fasta --id 0.9 --top_hits_only --userfields query+target --userout $2/table_abondance/table_abondance
+sed '1iOTU\tAnnotation' -i $2/table_abondance/table_abondance
+
+echo "----Fin du programme---- "
--- README.md
+++ README.md
@@ -3,5 +3,51 @@ Projet de métagénomique pour le master II de Bio-informatique.
 
 
 ```bash
-./16S_workflow.sh data/fastq/ results/
+./16S_workflow.sh fastq results
+```
+# Architecture du projet :
+Cette architecture doit être respectée pour pourvoir lancer le programme.
+```bash
+.
+├── 16S_workflow.sh
+├── databases
+│   ├── contaminants.fasta
+│   └── mock_16S_18S.fasta
+├── fastq
+│   ├── 1ng-25cycles-1_R1.fastq.gz
+│   ├── 1ng-25cycles-1_R2.fastq.gz
+│   ├── 1ng-25cycles-2_R1.fastq.gz
+│   ├── 1ng-25cycles-2_R2.fastq.gz
+│   ├── 1ng-25cycles-3_R1.fastq.gz
+│   ├── 1ng-25cycles-3_R2.fastq.gz
+│   ├── 1ng-30cycles-1_R1.fastq.gz
+│   ├── 1ng-30cycles-1_R2.fastq.gz
+│   ├── 1ng-30cycles-2_R1.fastq.gz
+│   ├── 1ng-30cycles-2_R2.fastq.gz
+│   ├── 1ng-30cycles-3_R1.fastq.gz
+│   ├── 1ng-30cycles-3_R2.fastq.gz
+│   ├── zero5ng-25cycles-1_R1.fastq.gz
+│   ├── zero5ng-25cycles-1_R2.fastq.gz
+│   ├── zero5ng-25cycles-2_R1.fastq.gz
+│   ├── zero5ng-25cycles-2_R2.fastq.gz
+│   ├── zero5ng-25cycles-3_R1.fastq.gz
+│   ├── zero5ng-25cycles-3_R2.fastq.gz
+│   ├── zero5ng-30cycles-1_R1.fastq.gz
+│   ├── zero5ng-30cycles-1_R2.fastq.gz
+│   ├── zero5ng-30cycles-2_R1.fastq.gz
+│   ├── zero5ng-30cycles-2_R2.fastq.gz
+│   ├── zero5ng-30cycles-3_R1.fastq.gz
+│   └── zero5ng-30cycles-3_R2.fastq.gz
+├── FastQC
+│   ├── fastqc
+│   ├── fastqc_icon.ico
+│   ├── Help
+
+└── soft
+    ├── AlienTrimmer.jar
+    ├── AlienTrimmer.java
+    ├── COPYING
+    ├── JarMaker.sh
+    ├── Makefile
+    └── vsearch
 ```
